b8817fb5b6486448aa7ee1ec309f465e83273d7b
Ruslan <rnagumanov57@gmail.com>
2025-05-15 21:28:15 +0500

    add_historical_conten

--- src/Pages/Preview/Preview.jsx
+++ src/Pages/Preview/Preview.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useNavigate } from 'react-router';
 import styles from './Preview.module.css';
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,24 @@
+import { useLocation } from 'react-router';
+
 import styles from './Header.module.css';
 
 const Header = () => {
+    const location = useLocation();
+    const isHistoricalPage = location.pathname === '/history';
+
     return (
         <>
             <div className={styles.header}>
-                <button className={styles.button_header}>
+                <button className={`${styles.button_header} ${isHistoricalPage ? styles.button_header_history : ''}`}>
                     Версия для <br /> слабовидящих
                 </button>
-                <button className={styles.button_header}>
+                <button className={`${styles.button_header} ${isHistoricalPage ? styles.button_header_history : ''}`}>
                     каталог <br /> экспонатов
                 </button>
-                <button className={styles.button_header}>
+                <button className={`${styles.button_header} ${isHistoricalPage ? styles.button_header_history : ''}`}>
                     аудиогид <br /> по приложению
                 </button>
-                <button className={styles.button_header}>ru</button>
+                <button className={`${styles.button_header} ${isHistoricalPage ? styles.button_header_history : ''}`}>ru</button>
             </div>
         </>
     );
